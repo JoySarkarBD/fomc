7e4f5e31838e91de4cb8fb2ae9866a58f20a1498
JoySarkarBD <developer.joysarkar@gmail.com>
2026-03-03 15:26:43 +0600

    feat(user): simplify getUser method by removing unnecessary user parameter

--- apps/api-gateway/src/user-management/user.controller.ts
+++ apps/api-gateway/src/user-management/user.controller.ts
@@ -186,10 +186,8 @@ export class UserController {
   @UseGuards(RolesGuard)
   @Roles("SUPER ADMIN", "DIRECTOR", "HR", "PROJECT MANAGER", "TEAM LEADER")
   @Get(":id")
-  async getUser(@GetUser() user: AuthUser, @Param() params: MongoIdDto) {
-    const result = await this.userService.getUser(
-      (user._id ?? user.id) as MongoIdDto["id"],
-    );
+  async getUser(@Param() params: MongoIdDto) {
+    const result = await this.userService.getUser(params.id);
     return result;
   }
 
